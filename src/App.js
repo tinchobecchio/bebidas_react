eb38bef7b1b8f211b4462ce10c8488bf8b2d953b
Martin Becchio <tincho.becchio@gmail.com>
2023-04-09 21:17:20 -0300

    colocando una receta en el state

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import Formulario from "./components/Formulario";
 import ListaRecetas from "./components/ListaRecetas";
 import CategoriasProvider from "./context/CategoriasContext";
 import RecetasProvider from "./context/RecetasContext";
+import ModalProvider from "./context/ModalContext"
 
 function App() {
   return (
     <CategoriasProvider>
       <RecetasProvider>
-        <Header />
+        <ModalProvider>
 
-        <div className="container my-5">
-          <div className="row">
-            <Formulario />
+          <Header />
+
+          <div className="container my-5">
+
+            <div className="row">
+              <Formulario />
+            </div>
+
+            <ListaRecetas />
+
           </div>
-          <ListaRecetas />
-        </div>
+
+        </ModalProvider>
       </RecetasProvider>
     </CategoriasProvider>
   );
--- src/components/Receta.js
+++ src/components/Receta.js
@@ -1,6 +1,13 @@
-
+import { useContext } from "react";
+import { ModalContext } from "../context/ModalContext";
 
 const Receta = ({receta}) => {
+
+    // extraer los valores del context
+    const { guardarIdReceta} = useContext(ModalContext);
+
+
+
     return ( 
         <div className="col-md-4 mb-3">
             <div className="card">
@@ -12,6 +19,9 @@ const Receta = ({receta}) => {
                     <button
                         type="button"
                         className="btn btn-block btn-primary"
+                        onClick={() => {
+                            guardarIdReceta(receta.idDrink)
+                        }}
                     >Ver Receta</button>
                 </div>
             </div>
